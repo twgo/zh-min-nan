7f476d53257aec74858896ea4aa0e0f4b3c8da11
leo424y <leo424y@gmail.com>
2018-08-03 08:43:40 +0800

    10000 to go

--- wiki_save.rb
+++ wiki_save.rb
@@ -2,18 +2,21 @@
 # gem install wikipedia-client
 require 'wikipedia'
 
+resource = 'input/index_720_10000.txt'
+
 Wikipedia.configure {
   domain 'zh-min-nan.wikipedia.org'
   path   'w/api.php'
 }
 
-File.open('index_720.txt', 'r').each_line do |line|
+File.open(resource, 'r').each_line do |line|
   begin
     data = line.chomp.split(/:/)
     number = data[0..1]
     name = data[2..-1]
     page = Wikipedia.find(name)
     File.open('data/' + number.join('__') + '.txt', 'w') {|f| f.write(page.title+"\n"+page.summary) }
+    File.open('data/' + number.join('__') + '_full.txt', 'w') {|f| f.write(page.title+"\n"+page.text) }
   rescue
   end
 end
